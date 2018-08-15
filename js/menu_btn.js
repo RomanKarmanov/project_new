6677454e1591eaca79645af43243ec378857bb52
Roman Karmanov <way_g@mail.ru>
2018-08-15 19:11:17 +0300

    burger_slider

--- js/menu_btn.js
+++ js/menu_btn.js
@@ -10,3 +10,34 @@ closeBtn.addEventListener("click", function(e){
 e.preventDefault();
 dropdown.style = 'display:none';
 });
+/*-------------------------------------------------------------------*/
+
+
+var teamName = document.querySelectorAll(".menu_btn");
+for (let i = 0; i < teamName.length; i++) {
+    teamName[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        for (let j = 0; j < teamName.length; j++) {
+            if (j !== i) {
+                teamName[j].closest('.accordeon_menu_item').classList.remove("active_menu_elem");
+
+            };
+        };
+        this.closest('.accordeon_menu_item').classList.add("active_menu_elem");
+        
+    });
+};
+var attempt = document.querySelectorAll(".squad_item_titel");
+for (let i = 0; i < attempt.length; i++) {
+    attempt[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        for (let j = 0; j < attempt.length; j++) {
+            if (j !== i) {
+                attempt[j].closest('.squad_item').classList.remove("squad_item_titel_active-two");
+                attempt[j].closest('.squad_item').classList.remove("squad_item_text_active-two");
+            };
+        };
+        this.closest('.squad_item').classList.add("squad_item_titel_active-two");
+        this.closest('.squad_item').classList.add("squad_item_text_active-two");
+    });
+};
